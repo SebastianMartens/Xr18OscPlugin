91ebf8cec7d505e162f1707ceaa74794e509cc0e
Sebastian Martens <sebastian.martens@hotmail.de>
2026-01-01 12:27:07 +0100

    Initial commit of Plugin and .Net solution Files

---
+++ src/Xr18OscApplication.cs
@@ -0,0 +1,19 @@
+namespace Loupedeck.Xr18OscPlugin;
+
+// This class can be used to connect the Loupedeck plugin to an application.
+
+public class Xr18OscApplication : ClientApplication
+{
+    public Xr18OscApplication()
+    {
+    }
+
+    // This method can be used to link the plugin to a Windows application.
+    protected override string GetProcessName() => "";
+
+    // This method can be used to link the plugin to a macOS application.
+    protected override string GetBundleName() => "";
+
+    // This method can be used to check whether the application is installed or not.
+    public override ClientApplicationStatus GetApplicationStatus() => ClientApplicationStatus.Unknown;
+}
