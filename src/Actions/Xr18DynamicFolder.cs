a9cae9cb6b3648dba550f2497805bdc8e41e5fb3
Sebastian Martens <sebastian.martens@hotmail.de>
2026-01-02 12:52:02 +0100

    adjustment dial command

---
+++ src/Actions/Xr18DynamicFolder.cs
@@ -0,0 +1,39 @@
+#nullable disable
+
+namespace Loupedeck.Xr18OscPlugin;
+
+using System.Collections.Generic;
+
+/// <summary>
+/// No useful content yet. Only for experiementing with dynamic folders.
+/// </summary>
+public class Xr18DynamicFolder : PluginDynamicFolder
+{     
+    public Xr18DynamicFolder()
+    {
+        DisplayName = "Dynamic Folder by Bart";
+        GroupName = "Barts Plugin";            
+    }
+
+    public override PluginDynamicFolderNavigation GetNavigationArea(DeviceType _) => base.GetNavigationArea(_);
+
+    public override IEnumerable<string> GetButtonPressActionNames(DeviceType deviceType)
+    {
+       return new[] {
+            PluginDynamicFolder.NavigateUpActionName,
+            CreateCommandName("7"),
+            CreateCommandName("8"),
+            CreateCommandName("9"),
+            CreateCommandName("."),
+            CreateCommandName("4"),
+            CreateCommandName("5"),
+            CreateCommandName("6"),
+            CreateCommandName("0"),
+            CreateCommandName("1"),
+            CreateCommandName("2"),
+            CreateCommandName("3")
+        };
+    }
+
+    public override bool Activate() => base.Activate();
+}
