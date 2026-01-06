3ebcd1f3b7715bd90aeaa41b05ff90439cf25581
Sebastian Martens <sebastian.martens@hotmail.de>
2026-01-06 17:11:06 +0100

    fixed comment

--- src/Actions/SendOscMessageFloatCommand.cs
+++ src/Actions/SendOscMessageFloatCommand.cs
@@ -1,14 +1,11 @@
 namespace Loupedeck.Xr18OscPlugin;
 
-/// <summary>
-/// This action command can be used to send any integer value to the mixer via OSC.
+/// This action command can be used to send any floating-point value to the mixer via OSC.
 /// Loupedeck itself supports things like toggles or grouping multiple commands together so this
 /// command is quite powerful if you know what OSC messages to send.
 /// 
-/// Examples: Address: /ch/01/dyn/on Value: 0 => Switch off the compressor on channel 1
-///           Address: /ch/02/eq/on Value: 1 => Switch on the EQ on channel 2
-///           Address: /headamp/01/phantom Value: 1 => Enable phantom power on preamp of channel 1
-/// </summary>
+/// Examples: Address: /ch/01/mix/pan   Value: 0.6   => Pan channel 1 slightly to the right
+///           Address: /ch/02/mix/fader Value: 0.3 => Set channel 2 fader level to "medium"
 public class SendOscMessageFloatCommand : ActionEditorCommand
 {
     public SendOscMessageFloatCommand()
