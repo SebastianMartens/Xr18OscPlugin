cc6d8bd2cb1db25cb5b0656623029fa3c20d7d44
Sebastian Martens <s.martens@kruss.de>
2026-04-07 11:45:04 +0200

    removed some useless tests

--- tests/Xr18OscPlugin.Tests/Domain/MainLrBusTests.cs
+++ tests/Xr18OscPlugin.Tests/Domain/MainLrBusTests.cs
@@ -9,34 +9,6 @@ public class MainLrBusTests
     private readonly Mock<IOscClient> _oscClientMock = new();
 
     [Fact]
-    public void MainLrBus_DefaultName()
-    {
-        var main = new MainLrBus(_oscClientMock.Object);
-        Assert.Equal("Main LR", main.Name.Value);
-    }
-
-    [Fact]
-    public void MainLrBus_DefaultIsOn_IsTrue()
-    {
-        var main = new MainLrBus(_oscClientMock.Object);
-        Assert.True(main.IsOn.Value);
-    }
-
-    [Fact]
-    public void MainLrBus_DefaultFaderLevel_IsZero()
-    {
-        var main = new MainLrBus(_oscClientMock.Object);
-        Assert.Equal(0.0f, main.MainFaderLevel.Value);
-    }
-
-    [Fact]
-    public void MainLrBus_DefaultPan_IsZero()
-    {
-        var main = new MainLrBus(_oscClientMock.Object);
-        Assert.Equal(0.0f, main.Pan.Value);
-    }
-
-    [Fact]
     public void MainLrBus_RegistersCorrectOscAddresses()
     {
         _ = new MainLrBus(_oscClientMock.Object);
